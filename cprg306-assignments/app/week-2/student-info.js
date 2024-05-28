b63862c62fdd17a604fd34a7f78d617734f2e94c
jasmeet1626 <Jasmeet.kaur01@edu.sait.ca>
2024-05-27 18:30:24 -0600

    link

--- cprg306-assignments/app/week-2/student-info.js
+++ cprg306-assignments/app/week-2/student-info.js
@@ -5,6 +5,9 @@ export default function StudentInfo() {
   return (
     <div>
       <h1>Jasmeet Kaur</h1>
+      <Link href="https://github.com/jasmeet1626/cprg306/tree/main/cprg306-assignments">
+        GitHub
+      </Link>
     </div>
   );
 }
