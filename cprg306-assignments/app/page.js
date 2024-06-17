0f0d298cb7b2c6bc11253c49efe8e79a9a59a461
jasmeet1626 <123598377+jasmeet1626@users.noreply.github.com>
2024-06-17 12:58:03 -0600

    Update page.js

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -5,6 +5,9 @@ export default function Home() {
       <div className="flex flex-col">
         <h1>CPRG 306: Web Development 2 - Assignments</h1>
         <Link href="/week-2">week-2</Link>
+        <Link href="/week-3">week-3</Link>
+        <Link href="/week-4">week-4</Link>
+        <Link href="/week-5">week-5</Link>
       </div>
     </main>
   );
