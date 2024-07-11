057839cb4b1adf6d3562f282ce5fd6915f795865
jasmeet1626 <123598377+jasmeet1626@users.noreply.github.com>
2024-07-11 17:26:30 -0600

    Update page.js

--- cprg306-assignments/app/page.js
+++ cprg306-assignments/app/page.js
@@ -9,7 +9,8 @@ export default function Home() {
         <Link href="/week-4">week-4</Link>
         <Link href="/week-5">week-5</Link>
         <Link href="/week-6">week-6</Link>
-        <Link href="/weel-7">weel-7</Link>
+        <Link href="/week-7">week-7</Link>
+        <Link href="/week-8">week-8</Link>
       </div>
     </main>
   );
